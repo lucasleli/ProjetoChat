3f8a949886071c8c295bf2960662fc3ed8a81402
LUCAS DOS SANTOS LELI <lucas.leli10@uninove.edu.br>
2019-04-22 23:02:28 -0300

    Criação do arquivo Readme

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 require_once("config.php");
 
 //////////////////////////////////////////
+
 /*
 $search = Usuario::search("pe"); //Realiza pesquisa de usuários pelo login
 
@@ -10,17 +11,23 @@ echo json_encode($search);
 */
 
 ////////////////////////////////////////
+
 /*
 $usuario = new Usuario(); // Realiza a pesquisa por cadastro por meio de autenticação Login/Senha
 $usuario->login("lucas", "1997");
 
 echo $usuario;
 */
+
 ///////////////////////////////////////
 
-$aluno = new Usuario("teste2", "484s");
+/*
+$aluno = new Usuario("teste2", "484s"); // Cadastra um novo usuário, precisando adiocionar apenas o LOGIN e SENHA
 
 $aluno->insert();
 
 echo $aluno;
+*/
+
+
  ?>
