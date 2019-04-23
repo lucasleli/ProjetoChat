b20668c573dc7c9616bee66d4db7e88461839600
LUCAS DOS SANTOS LELI <lucas.leli10@uninove.edu.br>
2019-04-23 16:56:30 -0300

    atualizar

--- index.php
+++ index.php
@@ -21,13 +21,13 @@ echo $usuario;
 
 ///////////////////////////////////////
 
-/*
+
 $aluno = new Usuario("teste2", "484s"); // Cadastra um novo usuário, precisando adiocionar apenas o LOGIN e SENHA
 
 $aluno->insert();
 
 echo $aluno;
-*/
+
 
 
  ?>
